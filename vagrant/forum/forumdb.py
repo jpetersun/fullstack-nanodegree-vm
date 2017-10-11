b87104224d206d324baa0abea2a5a7f2f9feba72
James Peterson <jpeterson@einsteinmedical.com>
2017-10-11 14:04:07 -0700

    Add solutions from video exercises

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,14 +2,25 @@
 
 import datetime
 
+import psycopg2
+
 POSTS = [("This is the first post.", datetime.datetime.now())]
 
 def get_posts():
   """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  conn = psycopg2.connect("dbname=forum")
+  cur = conn.cursor()
+  posts = cur.execute("select * from posts")
+  cur.close()
+  conn.close()    
+  return posts
 
 def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
-
+# POSTS.append((content, datetime.datetime.now()))
+  conn = psycopg2.connect("dbname=forum")
+  cur = conn.cursor()
+  cur.execute("insert into posts (content) values (%s)", (content,))
+  cur.close()
+  conn.close()
 
